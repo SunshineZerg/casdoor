72f2a085714349ba4c6b23a6ebcc44112d6dbb4a
8208201214 <8208201214@csu.edu.cn>
2022-10-28 12:22:28 +0800

    test

--- web/src/AdapterListPage.js
+++ web/src/AdapterListPage.js
@@ -26,10 +26,10 @@ import BaseListPage from "./BaseListPage";
 
 class AdapterListPage extends BaseListPage    {
   newAdapter() {
-    const randomName =Setting.getRandomName  ();
+    const randomName =Setting.getRandomName            ();
     return {
       owner: "built-in",
-      name: `adapter_${randomName}`,
+      name: `adapter_${randomName}` ,
       createdTime: moment().format(),
       organization: "built-in",
       type: "Database",
